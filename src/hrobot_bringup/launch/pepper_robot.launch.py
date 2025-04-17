0ecac5d723d1c3aec433817b77f8cd0d1a018a01
michailtam <mi.tamvak@gmail.com>
2025-04-18 01:08:43 +0300

    Gazebo plugins added and xacro files adapted.

--- src/hrobot_bringup/launch/pepper_robot.launch.py
+++ src/hrobot_bringup/launch/pepper_robot.launch.py
@@ -9,11 +9,11 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     # Paths
     pkg_share_description = FindPackageShare(package='hrobot_description').find('hrobot_description')
-    file_urdf = os.path.join(pkg_share_description, 'urdf', 'pepper_robot_fixed.urdf.xacro')
-    file_rviz_config = os.path.join(pkg_share_description, 'rviz', 'urdf_config.rviz')
+    urdf_file = os.path.join(pkg_share_description, 'urdf', 'pepper_robot_fixed.urdf.xacro')
+    rviz_config_file = os.path.join(pkg_share_description, 'rviz', 'urdf_config.rviz')
 
     # Generate robot description from xacro
-    robot_description = ParameterValue(Command(['xacro ', file_urdf]), value_type=str)
+    robot_description = ParameterValue(Command(['xacro ', urdf_file]), value_type=str)
 
     # Robot state publisher node
     robot_state_publisher_node = Node(
@@ -32,7 +32,7 @@ def generate_launch_description():
     rviz2_node = Node(
         package="rviz2",
         executable="rviz2",
-        arguments=['-d', file_rviz_config],
+        arguments=['-d', rviz_config_file],
         output="screen"
     )
 
